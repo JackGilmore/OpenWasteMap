baa900f1b13f58abefdf979f7cd17b3388ee5a1c
JackGilmore <gilmorejackc@gmail.com>
2021-08-07 21:18:59 +0100

    Add serilog for logging

--- OpenWasteMapUK/OpenWasteMapUK/Program.cs
+++ OpenWasteMapUK/OpenWasteMapUK/Program.cs
@@ -1,6 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace OpenWasteMapUK
 {
@@ -8,11 +10,30 @@ namespace OpenWasteMapUK
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.File("App_Data/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Web Host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- OpenWasteMapUK/OpenWasteMapUK/Repositories/DataRepository.cs
+++ OpenWasteMapUK/OpenWasteMapUK/Repositories/DataRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using OpenWasteMapUK.Models;
 using RestSharp;
@@ -20,18 +21,16 @@ namespace OpenWasteMapUK.Repositories
     {
         private readonly ApplicationDbContext _dbContext;
         private readonly IServiceScopeFactory _serviceScopeFactory;
-        public DataRepository(ApplicationDbContext dbContext, IServiceScopeFactory serviceScopeFactory)
+        private readonly ILogger<IDataRepository> _logger;
+        public DataRepository(ApplicationDbContext dbContext, IServiceScopeFactory serviceScopeFactory, ILogger<IDataRepository> logger)
         {
             _dbContext = dbContext;
             _serviceScopeFactory = serviceScopeFactory;
+            _logger = logger;
         }
         public async Task<IEnumerable<OsmElement>> GetElementsFromCache()
         {
             var elements = await _dbContext.OsmElements.AsNoTracking().ToListAsync();
-
-            //var oldestElement = elements.OrderBy(e => e.TimeStamp).FirstOrDefault();
-
-            //_ = Task.Run(() => CacheAgeCheck(oldestElement));
 
             _ = CacheAgeCheck();
 
@@ -48,20 +47,20 @@ namespace OpenWasteMapUK.Repositories
                 throw new NullReferenceException("DbContext init failed");
             }
 
-            Console.WriteLine("Running cache refresh...");
+            _logger.LogInformation("Running cache refresh...");
             IRestClient client = new RestClient("https://overpass.kumi.systems/api/interpreter");
             IRestRequest request = new RestRequest(Method.POST);
             request.AddParameter("application/x-www-form-urlencoded; charset=UTF-8", "data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A(%0A++node%5B%7E%22%5Eowner%22%7E%22Council%22,i%5D%5B%22recycling_type%22%3D%22centre%22%5D(49.88,-8.39,61.06,2.47)%3B%0A++way%5B%7E%22%5Eowner%22%7E%22Council%22,i%5D%5B%22recycling_type%22%3D%22centre%22%5D(49.88,-8.39,61.06,2.47)%3B%0A)%3B%0Aout+body%3B%0A%3E%3B%0Aout+skel+qt%3B", ParameterType.RequestBody);
 
-            Console.WriteLine("Querying OSM...");
+            _logger.LogInformation("Querying OSM...");
             IRestResponse response = await client.ExecuteAsync(request);
 
             if (!response.IsSuccessful)
             {
-                Console.WriteLine($"Cache refresh fail: {response.StatusCode} {response.Content}");
+                _logger.LogWarning($"Cache refresh fail: {response.StatusCode} {response.Content}");
             }
 
-            Console.WriteLine($"Got response back from OSM: {response.StatusCode}");
+            _logger.LogInformation($"Got response back from OSM: {response.StatusCode}");
             var osmResponse = JsonConvert.DeserializeObject<OsmResponse>(response.Content);
 
             try
@@ -76,7 +75,7 @@ namespace OpenWasteMapUK.Repositories
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                _logger.LogCritical(e.Message);
                 throw;
             }
         }
