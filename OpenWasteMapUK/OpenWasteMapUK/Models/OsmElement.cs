baa22676cd36c5ab65bacbe784e808851fb33186
JackGilmore <gilmorejackc@gmail.com>
2021-08-07 21:52:12 +0100

    List and cache stats

--- OpenWasteMapUK/OpenWasteMapUK/Models/OsmElement.cs
+++ OpenWasteMapUK/OpenWasteMapUK/Models/OsmElement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OpenWasteMapUK.Models
 {
@@ -8,10 +9,12 @@ namespace OpenWasteMapUK.Models
         public string Type { get; set; }
         public long Id { get; set; }
         public List<long> Nodes { get; set; }
-        public Dictionary<string,string> Tags { get; set; }
+        public Dictionary<string, string> Tags { get; set; }
         public double Lat { get; set; }
         public double Lon { get; set; }
         public DateTime TimeStamp { get; set; }
+        [NotMapped]
+        public string OsmLink => $"https://openstreetmap.org/{Type}/{Id}";
 
         public OsmElement()
         {
